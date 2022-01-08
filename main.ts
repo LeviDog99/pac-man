afcf14337e5dba802e1a43bba931ff5c28971f12
LeviDog99 <83308663+LeviDog99@users.noreply.github.com>
2022-01-08 16:28:29 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
+let Compass = 0
 let V2 = 0
 basic.forever(function () {
+    Compass = input.compassHeading()
     while (false) {
         V2 = input.soundLevel()
     }
-    pacman.ghost(0, "ghosts", MyEnum.One)
 })
